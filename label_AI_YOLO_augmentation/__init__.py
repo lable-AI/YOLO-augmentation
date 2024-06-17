75a702c6088116946b79982eeaab1f517d6aa3ef
Gerbylev <gerbylev.oleg@gmail.com>
2024-06-17 23:58:45 +0300

    add noise

--- label_AI_YOLO_augmentation/__init__.py
+++ label_AI_YOLO_augmentation/__init__.py
@@ -1,3 +1,3 @@
 from .reader import ImageAugmentationBuilder, augment_with_yolo_txt
-
-__all__ = ["ImageAugmentationBuilder", "augment_with_yolo_txt"]
+from .functions import HSV_image, add_noise_to_image
+__all__ = ["ImageAugmentationBuilder", "augment_with_yolo_txt", "HSV_image", "add_noise_to_image"]
